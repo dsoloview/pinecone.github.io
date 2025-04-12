2e28165cf2f1543482ba556b5c598eb41ed3f715
d.solovev <d.solovev@nexters.com>
2025-04-12 11:07:35 +0200

    линт

--- src/pages/private-event/index.vue
+++ src/pages/private-event/index.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import PageTitle from "~/entities/PageTitle/PageTitle.vue";
-import * as v from "valibot";
-import { ErrorMessage, Field } from "vee-validate";
 
 definePageMeta({
   name: "private-event",
 });
 
-const schema = v.object({
-  firstName: v.string(),
-  lastName: v.string(),
-  email: v.pipe(v.string(), v.email()),
-});
 const { t } = useI18n();
 </script>
 
@@ -21,14 +14,6 @@ const { t } = useI18n();
       t("pages.private-event.title")
     }}</PageTitle>
   </div>
-  <Form :validation-schema="schema">
-    <Field name="firstName" />
-    <ErrorMessage name="firstName" />
-    <Field name="lastName" />
-    <ErrorMessage name="lastName" />
-    <Field name="email" />
-    <ErrorMessage name="email" />
-  </Form>
 </template>
 
 <style scoped>
